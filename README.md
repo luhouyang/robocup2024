c2e1d72f58bf48c4d12949fd3309e5a8f27d26cc
monolith <monolith@LurkMonolith.myguest.virtualbox.org>
2024-08-19 01:15:03 +0800

    description files added

--- README.md
+++ README.md
@@ -8,3 +8,30 @@
 ## Follow the Setup Notes
 
 * [Setup Notes](https://github.com/luhouyang/robocup2024/blob/main/notes.md)
+
+## VS Code Preferences (User JSON)
+
+* yapf (EeyoreLee)
+
+* XML (Red Hat)
+
+```json
+{
+  "workbench.colorTheme": "Default High Contrast",
+  "[python]": {
+    "editor.formatOnSaveMode": "file",
+    "editor.formatOnSave": true,
+    "editor.defaultFormatter": "eeyore.yapf",
+    "editor.formatOnType": false
+  },
+  "redhat.telemetry.enabled": true,
+  "[xml]": {
+    "editor.defaultFormatter": "redhat.vscode-xml",
+    "editor.tabSize": 4,
+    "editor.trimAutoWhitespace": true,
+    "editor.formatOnSaveMode": "file",
+    "editor.formatOnSave": true
+  },
+  "xml.format.maxLineWidth": 120,
+}
+```
--- src/main_package/launch/rsp.launch.py
+++ src/main_package/launch/rsp.launch.py
@@ -19,25 +19,23 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory(package_name))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
-    )
 
+    # Create a robot_state_publisher node
+    params = {
+        'robot_description': robot_description_config.toxml(),
+        'use_sim_time': use_sim_time
+    }
+    node_robot_state_publisher = Node(package='robot_state_publisher',
+                                      executable='robot_state_publisher',
+                                      output='screen',
+                                      parameters=[params])
 
     # Launch!
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-
+        DeclareLaunchArgument('use_sim_time',
+                              default_value='false',
+                              description='Use sim time if true'),
         node_robot_state_publisher
     ])
